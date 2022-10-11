8776af418d3d548b08402f59f985e654fdbacad9
hanifagitfit <farhatunansari@gmail.com>
2022-10-11 13:04:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import divider from "./images/pattern-divider-desktop.svg";
+import dice from "./images/icon-dice.svg";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [text, setText] = useState([]);
+
+  const fetchAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+
+    console.log(data);
+    setText(data.slip);
+  }
+
+  useEffect(() => {
+    fetchAdvice()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="advice">
+        <h4>advice #{text.id} </h4>
+        <p>{text.advice}</p>
+      </div>
+      <div className="buttonArea">
+        <img id="divider" src={divider} alt="" />
+        <div className="palybtn" onClick={fetchAdvice}>
+          <img src={dice} alt="" />
+        </div>
+      </div>
     </div>
   );
 }
